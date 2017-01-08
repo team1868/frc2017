e088eec9ad468a0e4f76f0764c706788449c4af6
Maggie Wang <maggi3wang@gmail.com>
2017-01-07 20:41:18 -0800

    First commit

---
+++ src/Robot.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <IterativeRobot.h>
+#include <LiveWindow/LiveWindow.h>
+#include <SmartDashboard/SmartDashboard.h>
+
+class Robot: public frc::IterativeRobot {
+
+public:
+	void RobotInit() {
+
+	}
+
+	void AutonomousInit() override {
+
+	}
+
+	void AutonomousPeriodic() {
+
+	}
+
+	void TeleopInit() {
+
+	}
+
+	void TeleopPeriodic() {
+
+	}
+
+	void TestPeriodic() {
+
+	}
+};
+
+START_ROBOT_CLASS(Robot)
